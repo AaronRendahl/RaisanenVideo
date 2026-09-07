d90875d98b70edc6bcc4b414cf7c7e998dbeb0df
Aaron Rendahl <rend0020@umn.edu>
2026-09-07 14:08:55 -0500

    fix comment

--- clean_tape.sh
+++ clean_tape.sh
@@ -2,7 +2,7 @@
 
 # Step 1. Use this script to convert a clean copy
 # Step 2. Open with LosslessCut. Convert to supported format if asked.
-# Step 3. Find frames to start and end on, and put into cut2.sh
+# Step 3. Find frames to start and end on, and put into tapename.txt
 # Step 4. Run process_tape.sh
 
 if [[ -z "$1" ]]; then

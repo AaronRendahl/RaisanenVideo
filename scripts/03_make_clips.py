fa5bf24505de4c28b26d80241f563f92d1746c3a
Aaron Rendahl <rend0020@umn.edu>
2026-09-14 10:01:04 -0500

    Switch to crf 22 fast

--- scripts/03_make_clips.py
+++ scripts/03_make_clips.py
@@ -341,7 +341,7 @@ def main():
                         "-map_metadata", f"{meta_idx}",
                         "-map_chapters", f"{meta_idx}",
                         "-movflags", "+faststart",
-                        "-c:v", "libx264", "-crf", "18", "-preset", "slow",
+                        "-c:v", "libx264", "-crf", "22", "-preset", "fast",
                         "-c:a", "aac", "-b:a", "192k",
                         str(output_mp4)
                     ])
@@ -361,7 +361,7 @@ def main():
                         "-map_chapters", "1",
                         "-movflags", "+faststart",
                         "-vf", vf_base,
-                        "-c:v", "libx264", "-crf", "18", "-preset", "slow",
+                        "-c:v", "libx264", "-crf", "22", "-preset", "fast",
                         "-c:a", "aac", "-b:a", "192k",
                         str(output_mp4)
                     ]
